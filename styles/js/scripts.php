35d059e33b512e044bef151a59efa2f41cc83f59
Curtis McHale <curtis.mchale@gmail.com>
2009-09-16 21:27:00 -0700

    added auto remove and add for search form fields

--- styles/js/scripts.php
+++ styles/js/scripts.php
@@ -1,1 +1,19 @@
 <!-- the sitewide javascript assests -->
+<script type="text/javascript">
+// auto removes the values in the search box
+$(document).ready(function(){
+	swapValues = [];
+	    $(".text").each(function(i){
+	        swapValues[i] = $(this).val();
+	        $(this).focus(function(){
+	            if ($(this).val() == swapValues[i]) {
+	                $(this).val("");
+	            }
+	        }).blur(function(){
+	            if ($.trim($(this).val()) == "") {
+	                $(this).val(swapValues[i]);
+	            }
+	        });
+	    });
+});
+</script>
