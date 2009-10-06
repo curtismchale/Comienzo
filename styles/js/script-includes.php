a4430cb795561d4d7d5d95bb5148fb368000942e
Curtis McHale <curtis.mchale@gmail.com>
2009-10-05 22:06:39 -0700

    added google code libraries and commented out so other coders can use if they want

--- styles/js/script-includes.php
+++ styles/js/script-includes.php
@@ -2,8 +2,10 @@
 
 <script src="http://www.google.com/jsapi" ></script>
         <script>
-			//Load jQuery
 			google.load("jquery", "1.3.2");
-			//Load jQuery UI
-			google.load("jqueryui", "1.7.2");
+			//google.load("jqueryui", "1.7.2");
+			//google.load("prototype", "1.6");
+			//google.load("scriptaculous", "1.8.2");
+			//google.load("mootools", "1.2.3");
+			//google.load("yui", "2.8.0r4");
 		</script>
