05229ea916a8dc7664000443a132774742743be0
Curtis McHale <curtis.mchale@gmail.com>
2009-10-10 18:47:01 -0700

    added search type to search box for fancy webkit search box

--- searchform.php
+++ searchform.php
@@ -1,5 +1,5 @@
 <form method="get" id="searchform" action="<?php bloginfo('url'); ?>/">
 	<label class="hidden" for="s"><?php _e(''); ?></label>
-	<input type="text" class="text" value="<?php the_search_query(); ?>" name="s" id="s" />
+	<input type="search" class="text" value="<?php the_search_query(); ?>" name="s" id="s" />
 	<input type="submit" id="searchsubmit" value="Search" />
 </form>
