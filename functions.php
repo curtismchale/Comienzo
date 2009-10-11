a159d5dc4c3da055c178a1df51aaaf5799ef744d
Curtis McHale <curtis.mchale@gmail.com>
2009-10-10 18:49:51 -0700

    registerd sidebars in functions file

--- functions.php
+++ functions.php
@@ -7,6 +7,22 @@ if ( function_exists('register_sidebar') ) {
        'after_title' => '</h2>',
    ));
 }
+if ( function_exists('register_sidebar2') ) {
+   register_sidebar(array(
+       'before_widget' => '<li id="%1$s" class="widget %2$s">',
+       'after_widget' => '</li>',
+       'before_title' => '<h2 class="widgettitle">',
+       'after_title' => '</h2>',
+   ));
+}
+if ( function_exists('register_sidebar3') ) {
+   register_sidebar(array(
+       'before_widget' => '<li id="%1$s" class="widget %2$s">',
+       'after_widget' => '</li>',
+       'before_title' => '<h2 class="widgettitle">',
+       'after_title' => '</h2>',
+   ));
+}
 ?>
 <?php
 function theme_queue_js(){
