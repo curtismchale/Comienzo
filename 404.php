a5bdb07b45372367c76291caa7029ea7cb210a10
Curtis McHale <curtis.mchale@gmail.com>
2009-10-28 20:02:54 -0700

    added 404 page specific id

--- 404.php
+++ 404.php
@@ -1,6 +1,10 @@
 <?php get_header(); ?>
 
+    <div id="404">
+
 		<h2>Error 404 - Not Found</h2>
+                
+    </div><!-- end div id="404" -->
 
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
