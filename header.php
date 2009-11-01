cae291c068c07e9e8440bd1ef87b9b77c5e63307
Curtis McHale <curtis.mchale@gmail.com>
2009-10-31 20:25:53 -0700

    cleaned up header formating

--- header.php
+++ header.php
@@ -3,48 +3,50 @@
 
 	<head>
 		
-		<title>
-			<?php if (is_home()) { echo bloginfo('name');
-			} elseif (is_404()) {
-			echo '404 Not Found';
-			} elseif (is_category()) {
-			echo 'Category:'; wp_title('');
-			} elseif (is_search()) {
-			echo 'Search Results';
-			} elseif ( is_day() || is_month() || is_year() ) {
-			echo 'Archives:'; wp_title('');
-			} else {
-			echo wp_title('');
-			}
-			?>
-		</title>
+	    <title>
+		<?php if (is_home()) { echo bloginfo('name');
+		} elseif (is_404()) {
+		echo '404 Not Found';
+		} elseif (is_category()) {
+		echo 'Category:'; wp_title('');
+		} elseif (is_search()) {
+		echo 'Search Results';
+		} elseif ( is_day() || is_month() || is_year() ) {
+		echo 'Archives:'; wp_title('');
+		} else {
+		echo wp_title('');
+		}
+		?>
+	</title>
 
-	    <meta http-equiv="content-type" content="<?php bloginfo('html_type') ?>; charset=<?php bloginfo('charset') ?>" />
-		<meta name="description" content="<?php bloginfo('description') ?>" />
-		<?php if(is_search()) { ?>
-		<meta name="robots" content="noindex, nofollow" /> 
-	    <?php }?>
+	<meta http-equiv="content-type" content="<?php bloginfo('html_type') ?>; charset=<?php bloginfo('charset') ?>" />
+        <meta name="description" content="<?php bloginfo('description') ?>" />
+            
+	<?php if(is_search()) { ?>
+            <meta name="robots" content="noindex, nofollow" /> 
+	<?php }?>
 	
-		<link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_url'); ?>" media="screen" />
+	<link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_url'); ?>" media="screen" />
 		
         <!--[if IE 6]>
-			<link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo('stylesheet_url'); ?>/styles/css/ie6-styles.css" />
-		<![endif]-->
+            <link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo('stylesheet_url'); ?>/styles/css/ie6-styles.css" />
+	<![endif]-->
         
         <!--[if IE 7]>
-			<link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo('stylesheet_url'); ?>/styles/css/ie7-styles.css" />
-		<![endif]-->
+            <link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo('stylesheet_url'); ?>/styles/css/ie7-styles.css" />
+	<![endif]-->
 		
         <!--[if IE 8]>
-			<link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo('stylesheet_url'); ?>/styles/css/ie8-styles.css" />
-		<![endif]-->
+            <link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo('stylesheet_url'); ?>/styles/css/ie8-styles.css" />
+	<![endif]-->
 				
-		<link rel="alternate" type="application/rss+xml" title="<?php bloginfo('name'); ?> RSS Feed" href="<?php bloginfo('rss2_url'); ?>" />		
-		<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
+	<link rel="alternate" type="application/rss+xml" title="<?php bloginfo('name'); ?> RSS Feed" href="<?php bloginfo('rss2_url'); ?>" />		
+	
+        <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
 
-		<?php wp_head(); ?>
+	<?php wp_head(); ?>
 
-	</head>
+    </head>
 	
 <?php
    $url = explode('/',$_SERVER['REQUEST_URI']);
