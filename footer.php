06d60db628c21d16389bf90bee3e3d78d4bb53f0
Curtis McHale <curtis.mchale@gmail.com>
2009-10-31 20:32:27 -0700

    cleaned up indentation of footer

--- footer.php
+++ footer.php
@@ -1,10 +1,11 @@
-		<div class="footer">
-			<p class="copyright">&copy; <?php echo date("Y"); echo " "; echo bloginfo('name'); ?></p>
+            <div class="footer">
+                <p class="copyright">&copy; <?php echo date("Y"); echo " "; echo bloginfo('name'); ?></p>
 	
-			<p class="wordpress-info"><?php bloginfo('name'); ?> is proudly powered by <a href="http://wordpress.org/">WordPress</a> | <a href="<?php bloginfo('rss2_url'); ?>">Entries (RSS)</a> | <a href="<?php bloginfo('comments_rss2_url'); ?>">Comments (RSS)</a>.</p>
+                <p class="wordpress-info"><?php bloginfo('name'); ?> is proudly powered by <a href="http://wordpress.org/">WordPress</a> | <a href="<?php bloginfo('rss2_url'); ?>">Entries (RSS)</a> | <a href="<?php bloginfo('comments_rss2_url'); ?>">Comments (RSS)</a>.</p>
 		
-			<?php wp_footer(); ?>
-		</div><!-- div class="footer" -->
+                <?php wp_footer(); ?>
+	
+            </div><!-- div class="footer" -->
 
 	</div><!-- end div id="main-wrapper" -->
 	
