94eb95b404a79a6aab265c775b87a3a6c5e3708c
curtis mchale <curtis.mchale@gmail.com>
2009-12-06 09:27:20 -0800

    added comments to index

--- index.php
+++ index.php
@@ -25,7 +25,7 @@
 				<ul class="next-previous-links">
 					<li><?php next_posts_link('&laquo; Older Entries') ?></li>
 					<li><?php previous_posts_link('Newer Entries &raquo;') ?></li>
-				</ul>
+				</ul><!-- /.next-previous-links -->
 			
 			<?php else : ?>
 	
