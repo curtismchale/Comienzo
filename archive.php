80b16aeb9f9e714416a8e80b65de3506f8faf5e0
curtis mchale <curtis.mchale@gmail.com>
2009-12-06 09:26:14 -0800

    rewrote archive to match index and have better titles

--- archive.php
+++ archive.php
@@ -6,27 +6,31 @@
 
 		<?php if (have_posts()) : ?>
 
-		<?php $post = $posts[0]; // Hack. Set $post so that the_date() works. ?>
-		<?php /* If this is a category archive */ if (is_category()) { ?>
-		<h2>Archive for the &#8216;<?php single_cat_title(); ?>&#8217; Category</h2>
-		<?php /* If this is a tag archive */ } elseif( is_tag() ) { ?>
-		<h2>Posts Tagged &#8216;<?php single_tag_title(); ?>&#8217;</h2>
-		<?php /* If this is a daily archive */ } elseif (is_day()) { ?>
-		<h2>Archive for <?php the_time('F jS, Y'); ?></h2>
-		<?php /* If this is a monthly archive */ } elseif (is_month()) { ?>
-		<h2>Archive for <?php the_time('F, Y'); ?></h2>
-		<?php /* If this is a yearly archive */ } elseif (is_year()) { ?>
-		<h2>Archive for <?php the_time('Y'); ?></h2>
-		<?php /* If this is an author archive */ } elseif (is_author()) { ?>
-		<h2>Author Archive</h2>
-		<?php /* If this is a paged archive */ } elseif (isset($_GET['paged']) && !empty($_GET['paged'])) { ?>
-		<h2>Blog Archives</h2>
-		<?php } ?>
+			<div id="archive-title">
+			    <?php $post = $posts[0]; // Hack. Set $post so that the_date() works. ?>
+			    <?php /* If this is a category archive */ if (is_category()) { ?>
+			    <h2>Archive for the &#8216;<?php single_cat_title(); ?>&#8217; Category</h2>
+			    <?php /* If this is a tag archive */ } elseif( is_tag() ) { ?>
+			    <h2>Posts Tagged &#8216;<?php single_tag_title(); ?>&#8217;</h2>
+			    <?php /* If this is a daily archive */ } elseif (is_day()) { ?>
+			    <h2>Archive for <?php the_time('F jS, Y'); ?></h2>
+			    <?php /* If this is a monthly archive */ } elseif (is_month()) { ?>
+			    <h2>Archive for <?php the_time('F, Y'); ?></h2>
+			    <?php /* If this is a yearly archive */ } elseif (is_year()) { ?>
+			    <h2>Archive for <?php the_time('Y'); ?></h2>
+			    <?php /* If this is an author archive */ } elseif (is_author()) { ?>
+			    <h2>Author Archive</h2>
+			    <?php /* If this is a paged archive */ } elseif (isset($_GET['paged']) && !empty($_GET['paged'])) { ?>
+			    <h2>Blog Archives</h2>
+			    <?php } ?>
+			</div><!-- /#archive-title -->
+
 
 		<?php while (have_posts()) : the_post(); ?>
-				<h3 id="post-<?php the_ID(); ?>"><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h3>
+			
 			<div class="post" id="post-<?php the_ID(); ?>">
-	                   <div class="post-heading">
+	                  
+			   <div class="post-heading">
 	                       <p class="date-published"><?php the_time('M j Y') ?></p>                        
 	                       <h2 class="post-title"><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
 	                       <p class="post-author">by <?php the_author() ?></p>
