d0298d3602fafe91f8aec81f81a0e02137a4fc0f
curtis mchale <curtis.mchale@gmail.com>
2009-12-15 20:31:42 -0800

    only call pagination links if there is more than one page

--- assets/includes/post-navigation.php
+++ assets/includes/post-navigation.php
@@ -1,4 +1,8 @@
-<ul class="next-previous-links">
-    <li><?php next_posts_link('&laquo; Older Entries') ?></li>
-    <li><?php previous_posts_link('Newer Entries &raquo;') ?></li>
-</ul><!-- /.next-previous-links -->
+<?php if ($paged > 1) { ?>
+
+	<div class="navigation">
+		<div class="alignleft"><?php next_posts_link('&laquo; Older Entries') ?></div>
+        	<div class="alignright"><?php previous_posts_link('Newer Entries &raquo;') ?></div>
+	</div>
+
+<?php } ?>
