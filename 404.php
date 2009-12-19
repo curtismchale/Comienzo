db70989aa8a7b8b62e1384ea728cc7a6dbe0188c
curtis mchale <curtis.mchale@gmail.com>
2009-12-18 21:23:17 -0800

    cleaned up 404 page

--- 404.php
+++ 404.php
@@ -2,13 +2,13 @@
 
 <div id="content">
 
-    <div id="four-o-four">
+	<div id="four-o-four">
 
 		<h2>Error 404 - Not Found</h2>
 
-	    <?php get_sidebar(); ?>
+		<?php get_sidebar(); ?>
 
-    </div><!-- /#four-o-four -->
+	</div><!-- /#four-o-four -->
     
 </div><!-- /#content -->
 <?php get_footer(); ?>
