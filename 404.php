6c82c821c9ee9fad53ec57d1f663662ff0915580
curtis mchale <curtis.mchale@gmail.com>
2009-12-18 21:22:35 -0800

    made id validate

--- 404.php
+++ 404.php
@@ -2,13 +2,13 @@
 
 <div id="content">
 
-    <div id="404">
+    <div id="four-o-four">
 
 		<h2>Error 404 - Not Found</h2>
 
 	    <?php get_sidebar(); ?>
 
-    </div><!-- /#404 -->
+    </div><!-- /#four-o-four -->
     
 </div><!-- /#content -->
 <?php get_footer(); ?>
