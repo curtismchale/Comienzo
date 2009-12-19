f5bca080d31bb4f1f0941675d7a4d5a363f3030b
curtis mchale <curtis.mchale@gmail.com>
2009-12-18 21:36:01 -0800

    matched loop on search.php

--- search.php
+++ search.php
@@ -10,11 +10,17 @@
 	
 			<?php while (have_posts()) : the_post(); ?>
 	
-				<div class="post">
-					<h3 id="post-<?php the_ID(); ?>"><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h3>
-					<p><?php the_time('l, F jS, Y') ?></p>
-					<p><?php the_tags('Tags: ', ', ', '<br />'); ?> Posted in <?php the_category(', ') ?> | <?php edit_post_link('Edit', '', ' | '); ?>  <?php comments_popup_link('No Comments &#187;', '1 Comment &#187;', '% Comments &#187;'); ?></p>
-				</div><!-- /.post -->
+				<div class="post" id="post-<?php the_ID(); ?>">
+					<div class="post-heading">
+						<p class="date-published"><?php the_time('M j Y') ?></p>                        
+						<h2 class="post-title"><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
+						<p class="post-author">by <?php the_author() ?></p>
+					</div><!-- /.post-heading -->
+	
+					<?php the_content('Read the rest of this entry &raquo;'); ?>
+	
+					<p class="post-tags"><?php the_tags('Tags: ', ', ', '<br />'); ?> Posted in <?php the_category(', ') ?> | <?php edit_post_link('Edit', '', ' | '); ?>  <?php comments_popup_link('No Comments &#187;', '1 Comment &#187;', '% Comments &#187;'); ?></p>
+				</div><!-- /.post #post-phpstuffy -->
 	
 			<?php endwhile; ?>
 	
