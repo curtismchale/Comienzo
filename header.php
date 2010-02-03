9281b25b0334998cc542f00660c23bdf57f070a0
curtis mchale <curtis.mchale@gmail.com>
2010-02-02 18:58:27 -0800

    moved wp_head call so theme css takes precidence and cleaned up formatting on header.php

--- header.php
+++ header.php
@@ -3,7 +3,7 @@
 
 	<head>
 		
-	    	<title>
+		<title>
 			<?php if (is_home()) { echo bloginfo('name');
 			} elseif (is_404()) {
 			echo '404 Not Found';
@@ -20,11 +20,13 @@
 		</title>
 
 	<meta http-equiv="content-type" content="<?php bloginfo('html_type') ?>; charset=<?php bloginfo('charset') ?>" />
-        <meta name="description" content="<?php bloginfo('description') ?>" />
+    <meta name="description" content="<?php bloginfo('description') ?>" />
             
 	<?php if(is_search()) { ?>
-            <meta name="robots" content="noindex, nofollow" /> 
+		<meta name="robots" content="noindex, nofollow" /> 
 	<?php }?>
+	
+	<?php wp_head(); ?>
 	
 	<link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_url'); ?>" media="screen" />
 		
@@ -32,11 +34,9 @@
 		<link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo('stylesheet_directory'); ?>/assets/css/ie6-styles.css" />
 	<![endif]-->
 					
-	<link rel="alternate" type="application/rss+xml" title="<?php bloginfo('name'); ?> RSS Feed" href="<?php bloginfo('rss2_url'); ?>" />		
+    <link rel="alternate" type="application/rss+xml" title="<?php bloginfo('name'); ?> RSS Feed" href="<?php bloginfo('rss2_url'); ?>" />		
 	
-        <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
-
-	<?php wp_head(); ?>
+	<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
     
     </head>
 	
