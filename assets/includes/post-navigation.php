1e7f7a412549374cbf1cac7efd02fbadf5ecf065
curtismchale <curtis.mchale@gmail.com>
2010-02-23 19:48:01 -0800

    removed if paged call b/c it breaks pagination

--- assets/includes/post-navigation.php
+++ assets/includes/post-navigation.php
@@ -1,8 +1,4 @@
-<?php if ($paged > 1) { ?>
-
 	<div class="navigation">
 		<div class="alignleft"><?php next_posts_link('&laquo; Older Entries') ?></div>
         	<div class="alignright"><?php previous_posts_link('Newer Entries &raquo;') ?></div>
 	</div>
-
-<?php } ?>
