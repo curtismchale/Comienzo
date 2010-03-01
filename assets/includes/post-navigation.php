8cae4080013d7fc7ffe0a6c429ea19ef0a4f0eb1
curtismchale <curtis.mchale@gmail.com>
2010-02-28 18:00:44 -0800

    cleaned up tabs for spaces and extra spaces throughout the code

--- assets/includes/post-navigation.php
+++ assets/includes/post-navigation.php
@@ -1,4 +1,4 @@
-	<div class="navigation">
-		<div class="alignleft"><?php next_posts_link('&laquo; Older Entries') ?></div>
-        	<div class="alignright"><?php previous_posts_link('Newer Entries &raquo;') ?></div>
-	</div>
+<div class="navigation">
+    <div class="alignleft"><?php next_posts_link('&laquo; Older Entries') ?></div>
+    <div class="alignright"><?php previous_posts_link('Newer Entries &raquo;') ?></div>
+</div>
--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,22 +1,22 @@
 // sitewide javascript assests
 // auto removes the values in text field if they have value of text
 jQuery(document).ready(function(){
-	swapValues = [];
-	    jQuery(".text").each(function(i){
-	        swapValues[i] = jQuery(this).val();
-	        jQuery(this).focus(function(){
-	            if (jQuery(this).val() == swapValues[i]) {
-	                jQuery(this).val("");
-	            }
-	        }).blur(function(){
-	            if (jQuery.trim(jQuery(this).val()) == "") {
-	                jQuery(this).val(swapValues[i]);
-	            }
-	        });
-	    });
+    swapValues = [];
+        jQuery(".text").each(function(i){
+            swapValues[i] = jQuery(this).val();
+            jQuery(this).focus(function(){
+                if (jQuery(this).val() == swapValues[i]) {
+                    jQuery(this).val("");
+                }
+            }).blur(function(){
+                if (jQuery.trim(jQuery(this).val()) == "") {
+                    jQuery(this).val(swapValues[i]);
+                }
+            });
+        });
 });
 
 jQuery(document).ready(function() {
-	//comment form validation
-	jQuery("#commentform").validate();
+    //comment form validation
+    jQuery("#commentform").validate();
 });
