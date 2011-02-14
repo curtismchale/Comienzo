f6b7ba4633e40d5e6a344be131d0641522870ad5
curtismchale <curtis@curtismchale.ca>
2011-02-13 20:26:12 -0800

    switched to the html5 footer tag

--- footer.php
+++ footer.php
@@ -1,12 +1,12 @@
 <div class="clear"></div>
             </div><!-- /#content -->
 
-            <div class="footer">
+            <footer>
                 <p class="copyright">&copy; <?php echo date("Y"); echo " "; echo bloginfo('name'); ?></p>
 
                 <p class="wordpress-info"><?php bloginfo('name'); ?> is proudly powered by <a href="http://wordpress.org/">WordPress</a> | <a href="<?php bloginfo('rss2_url'); ?>">Entries (RSS)</a> | <a href="<?php bloginfo('comments_rss2_url'); ?>">Comments (RSS)</a>.</p>
 
-            </div><!-- /.footer -->
+            </footer><!-- /footer -->
 
     </div><!-- /#main-wrapper -->
 
