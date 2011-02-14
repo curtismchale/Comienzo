025aa6b17e2064540c3e35d54d2d9b2428101efb
curtismchale <curtis@curtismchale.ca>
2011-02-13 21:23:54 -0800

    changed registration so it's proper for the theme

--- assets/includes/admin/admin-options.php
+++ assets/includes/admin/admin-options.php
@@ -7,7 +7,7 @@ add_action( 'admin_menu', 'theme_options_add_page' );
  * Init plugin options to white list our options
  */
 function theme_options_init(){
-	register_setting( 'sample_options', 'sample_theme_options', 'theme_options_validate' );
+	register_setting( 'comienzo_options', 'comienzo_theme_options', 'theme_options_validate' );
 }
 
 /**
