6496c7abf11439666ced48b1603c00c419abbc76
curtismchale <curtis@curtismchale.ca>
2011-02-13 20:22:49 -0800

    * added text domain to theme * fixed the depricated function

--- searchform.php
+++ searchform.php
@@ -1,5 +1,5 @@
-<form method="get" id="searchform" action="<?php bloginfo('url'); ?>/">
-    <label class="hidden" for="s"><?php _e(''); ?></label>
+<form method="get" id="searchform" action="<?php home_url(); ?>/">
+    <label class="hidden" for="s"><?php _e('', 'comienzo'); ?></label>
     <input type="search" class="text" value="search" name="s" id="s" />
     <input type="submit" id="searchsubmit" value="Search" />
 </form>
