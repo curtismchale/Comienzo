14bcccab8c67c1c2969dfd2d0489b719c0c9cb1d
curtismchale <curtis@curtismchale.ca>
2011-08-22 11:01:10 -0700

    removed the google CDN stuff since I often end up stripping it anyway
    In the future I'll simply include it if needed. This will keep themes on
    a more recent version of jQuery anyway since WordPress bundles it and
    upgrades it.

--- assets/includes/add-js.php
+++ assets/includes/add-js.php
@@ -3,8 +3,6 @@
      // only need on the frontend of the site (most scripts)
     function jsNotAdmin() {
         if (!is_admin()){
-            wp_deregister_script('jquery');
-            wp_register_script('jquery', ("http://ajax.googleapis.com/ajax/libs/jquery/1.4.4/jquery.min.js"), false, '1.4.2',true);
             wp_enqueue_script('jquery');
             wp_enqueue_script('jqueryvalidate', get_template_directory_uri().'/assets/js/jquery.validate.min.js', array('jquery'), '1.0', true);
         }// end if is_admin new scripts go above this
@@ -19,15 +17,6 @@
         }// end if is_admin
     }
     add_action('wp_footer', 'restatement_footer_scripts');
-    // remove version number from jQuery
-    add_filter('script_loader_src','restatement_scripts_unversion');
-    function restatement_scripts_unversion($src) {
-        if( strpos($src,'ajax.googleapis.com') )
-            $src=remove_query_arg('ver', $src);
-        return $src;
-    }
-
-
     // scripts needed in admin (almost never)
     function jsInAdmin(){
 
