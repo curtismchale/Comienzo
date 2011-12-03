dc7dd35ec2027a5c457552806f5bfed0cf8a479c
curtismchale <curtis@curtismchale.ca>
2011-12-03 14:14:56 -0800

    added better default search message

--- 404.php
+++ 404.php
@@ -18,7 +18,8 @@
     <div id="content-wrapper">
 
       <h2>Error 404 - Not Found</h2>
-      <!-- TODO add default contact information here -->
+      <h5>Oops!! Looks like something went wrong. Please get in touch with the site <a href="mailto:<?php echo get_bloginfo('admin_email'); ?>">administrator</a> and we'll get this sorted out. You my also try to find what you're looking for with the saerch below.</h5>
+      <?php get_search_form(); ?>
 
     </div><!-- /#content-wrapper -->
 
