be7726d9518b6efc931c9b1b54f070d0de6d8af3
curtismchale <curtis@curtismchale.ca>
2011-12-19 20:56:50 -0800

    refactored JS
    updating the JS to current best practice which is something I've just been putting off for a while now.

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,24 +1,27 @@
-// sitewide javascript assests
-// auto removes the values in text field if they have value of text
-jQuery(document).ready(function(){
-    swapValues = [];
-        jQuery(".text").each(function(i){
-            swapValues[i] = jQuery(this).val();
-            jQuery(this).focus(function(){
-                if (jQuery(this).val() == swapValues[i]) {
-                    jQuery(this).val("");
-                }
-            }).blur(function(){
-                if (jQuery.trim(jQuery(this).val()) == "") {
-                    jQuery(this).val(swapValues[i]);
-                }
-            });
-        });
-});
+jQuery(document).ready(function($) {
 
-jQuery(document).ready(function() {
-    //comment form validation
-    jQuery("#commentform").validate();
-    // killing image rollover background
-    jQuery('img').parent('a').addClass('image');
+  //comment form validation
+  $("#commentform").validate();
+  // killing image rollover background
+  $('img').parent('a').addClass('image');
+
+  /**
+   * This automatically removes the text in the default
+   * seach field for the theme. Add CSS selectors to
+   * target fields from other contact/form plugins.
+   */
+  swapValues = [];
+  $(".text").each(function(i){
+      swapValues[i] = $(this).val();
+      $(this).focus(function(){
+          if ($(this).val() == swapValues[i]) {
+              $(this).val("");
+          }
+      }).blur(function(){
+          if ($.trim(jQuery(this).val()) == "") {
+              $(this).val(swapValues[i]);
+          }
+      });
+  });
+
 });
