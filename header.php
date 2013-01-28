36ae352a7a03a76189b612ff205cb865154597cc
curtismchale <curtis@curtismchale.ca>
2013-01-27 20:52:53 -0800

    - switched to wp_title
    any SEO plugin wants to take care of this anyway so I should just let
    them do it instead of trying to be all fancy

--- header.php
+++ header.php
@@ -16,21 +16,7 @@
 
     <head>
 
-        <title>
-            <?php if (is_home()) { echo bloginfo('name');
-            } elseif (is_404()) {
-            echo '404 Not Found';
-            } elseif (is_category()) {
-            echo 'Category:'; wp_title('');
-            } elseif (is_search()) {
-            echo 'Search Results';
-            } elseif ( is_day() || is_month() || is_year() ) {
-            echo 'Archives:'; wp_title('');
-            } else {
-            echo wp_title('');
-            }
-            ?>
-        </title>
+        <title><?php wp_title(); ?></title>
 
         <meta http-equiv="content-type" content="<?php bloginfo('html_type') ?>; charset=<?php bloginfo('charset') ?>" />
         <meta name="description" content="<?php bloginfo('description') ?>" />
