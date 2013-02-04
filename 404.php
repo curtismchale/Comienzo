ad9287899795f07d5c4535d148d1b72e97b166fb
curtismchale <curtis@curtismchale.ca>
2013-02-03 18:16:45 -0800

    added logging conditionally since I normally log in dev

--- 404.php
+++ 404.php
@@ -21,6 +21,24 @@
       <h5>Oops!! Looks like something went wrong. Please get in touch with the site <a href="mailto:<?php echo get_bloginfo('admin_email'); ?>">administrator</a> and we'll get this sorted out. You my also try to find what you're looking for with the saerch below.</h5>
       <?php get_search_form(); ?>
 
+	    <?php
+	    /**
+	     * I develop with logging so I just want my logging to be done in the theme if
+	     * at all possible.
+ 	     */
+	    if( function_exists( 'wptt_log_error' ) ){
+
+				$current_user = wp_get_current_user();
+
+				wptt_log_error(
+					'404 Page',
+					'Someone hit the 404 page',
+					array( 'user' => $current_user )
+				);
+
+			} // function_exists( 'wptt_log_error' )
+	    ?>
+
     </div><!-- /#content-wrapper -->
 
     <?php get_sidebar(); ?>
