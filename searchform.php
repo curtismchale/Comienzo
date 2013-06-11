bd5746632fbff9a38127142d664faaa9f760acf9
curtismchale <curtis@curtismchale.ca>
2013-06-11 15:01:49 -0700

    - changed type to text
    Always have issues with clients wanting a custom style here so I'm just
    dropping the search type.

--- searchform.php
+++ searchform.php
@@ -13,6 +13,6 @@
 ?>
 <form method="get" id="searchform" action="<?php home_url(); ?>/">
   <label class="hidden" for="s"><?php _e('', 'comienzo'); ?></label>
-  <input type="search" class="text" value="search" name="s" id="s" />
+  <input type="text" class="text" value="search" name="s" id="s" />
   <input type="submit" id="searchsubmit" value="Search" />
 </form>
